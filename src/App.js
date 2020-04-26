aec800302f2cdd6c6bc320be7953afc1eaf281c5
samedbilgili <59111728+samedbilgili@users.noreply.github.com>
2020-04-27 00:44:50 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -4,23 +4,100 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <LoginControl />
+      <LightControl />
+    </React.StrictMode>
   );
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  } else {
+    return <GuestGreeting />;
+  }
+}
+
+function UserGreeting() {
+  return (<p><h1>&nbsp;Welcome back!</h1></p>);
+}
+
+function GuestGreeting() {
+  return (<p><h1>&nbsp;Please Sign Up!</h1></p>);
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    let button;
+    if (this.state.isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+class LightControl extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isLightOn: true }
+
+    this.handleLightButtonClick = this.handleLightButtonClick.bind(this);
+  }
+
+  handleLightButtonClick() {
+    console.log('geldii');
+    this.setState(state => ({
+      isLightOn: !state.isLightOn
+    }));
+  }
+
+  render() {
+    return (<LightButton onClick={this.handleLightButtonClick} isLightOn={this.state.isLightOn} />);
+  };
+}
+
+function LoginButton(props) {
+  return (<button onClick={props.onClick} style={{ width: "250px", height: "50px", backgroundColor: "green", color: "white" }}>Login</button>);
+}
+
+function LogoutButton(props) {
+  return (<button onClick={props.onClick} style={{ width: "250px", height: "50px", backgroundColor: "red", color: "white" }}>Logout</button>);
+}
+
+
+function LightButton(props) {
+  if (props.isLightOn) {
+    return (<button onClick={props.onClick} style={{ backgroundColor: "lightblue", width: "250px", height: "50px" }}>Işığı Kapat</button>);
+  } else {
+    return (<button onClick={props.onClick} style={{ backgroundColor: "black", color: "white", width: "250px", height: "50px" }}>Işığı Aç</button>);
+  }
 }
 
 export default App;
